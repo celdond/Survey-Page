08e1a26114e6acf9e56c998b9b2d210e1c754f2c
celdond <celdond@gmail.com>
2023-08-24 21:09:35 -0700

    last adjustments for backend set-up

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,6 @@
 import app from './app';
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const port = process.env.PORT;
+const port = 3010;
 
 app.listen(port, () => {
   console.log(`[Server]: I am running at https://localhost:${port}`);
